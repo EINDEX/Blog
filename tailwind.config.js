3ca2941e283a7481330b79644d2b3873ae15f7c8
EINDEX <eindex.me@outlook.com>
2022-11-05 11:08:19 +0800

    update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
     content: ["./templates/**/*.html", "./themes/**/*.html",  "./themes/**/*.html"],
     theme: {
       screens: {
+        'md': {max: '768px'},
         'sm': {max: '640px'},
       },
       extend: {},
