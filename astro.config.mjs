8e41ca88f1c517bbed337e5629fa85219289631c
EINDEX <snowstarlbk@gmail.com>
2023-03-21 16:33:47 +0800

    feat: add pwa
    Signed-off-by: EINDEX <snowstarlbk@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,34 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: "https://eindex.me",
   integrations: [
-    AstroPWA(),
+    AstroPWA({
+      base: '/',
+      scope: '/',
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      workbox: { navigateFallback: "/404" },
+      manifest: {
+        name: "EINDEX's Blog",
+        short_name: "EINDEX's Blog",
+        description: "EINDEX's Blog, share my thoughts and visions.",
+        icons: [
+          {
+            src: "/favicon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#FFFFFF",
+        background_color: "#FFFFFF",
+        display: "standalone",
+        prefer_related_applications: false,
+      },
+    }),
     astroI18next(),
     sitemap(),
     mdx({
