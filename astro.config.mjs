833a45c82129a50e13d30f4ac57070aff0476b38
EINDEX <snowstarlbk@gmail.com>
2023-03-22 10:09:44 +0800

    fix: try to fix auto reload when pwa enable

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://eindex.me",
+  site: "https://staging.eindex.me",
   integrations: [
     AstroPWA({
       base: "/",
