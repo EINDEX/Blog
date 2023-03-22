8b320715c4458eb1edaa9a8e466b6eedb5ee5a2b
EINDEX <snowstarlbk@gmail.com>
2023-03-22 10:24:28 +0800

    fix: remove pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import rehypeKatex from "rehype-katex";
 import astroI18next from "astro-i18next";
 import remarkMath from "remark-math";
 import sitemap from "@astrojs/sitemap";
-import AstroPWA from "@vite-pwa/astro";
 import tailwind from "@astrojs/tailwind";
 
 // import cloudflare from "@astrojs/cloudflare";
@@ -15,35 +14,35 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: "https://staging.eindex.me",
   integrations: [
-    AstroPWA({
-      base: "/",
-      scope: "/",
-      registerType: "autoUpdate",
-      manifest: {
-        name: "EINDEX's Blog",
-        short_name: "EINDEX's Blog",
-        icons: [
-          {
-            src: "/favicon-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/favicon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-        theme_color: "#323232",
-        background_color: "#323232",
-        display: "standalone",
-        prefer_related_applications: false,
-      },
-      workbox: {
-        navigateFallback: "/404",
-        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
-      },
-    }),
+    // AstroPWA({
+    //   base: "/",
+    //   scope: "/",
+    //   registerType: "autoUpdate",
+    //   manifest: {
+    //     name: "EINDEX's Blog",
+    //     short_name: "EINDEX's Blog",
+    //     icons: [
+    //       {
+    //         src: "/favicon-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/favicon-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     theme_color: "#323232",
+    //     background_color: "#323232",
+    //     display: "standalone",
+    //     prefer_related_applications: false,
+    //   },
+    //   workbox: {
+    //     navigateFallback: "/404",
+    //     globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+    //   },
+    // }),
     astroI18next(),
     sitemap(),
     mdx({
