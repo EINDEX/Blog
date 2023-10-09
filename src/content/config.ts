23791b9f55e40e6f42a8b1b1bdac445a461d80da
EINDEX <snowstarlbk@gmail.com>
2023-10-09 23:41:12 +0800

    feat: enhance

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,7 @@ const basicSchema = {
   description: z.string().nullable().optional(),
   draft: z.boolean().nullable().optional().default(true),
   pssoes: z.array(z.string()).optional().default([]),
+  translateBy: z.string().nullable().optional().default(null),
 };
 
 const postSchema = z.object({
@@ -17,7 +18,6 @@ const postSchema = z.object({
   date: z
     .date()
     .or(z.string().transform((str) => (str ? new Date(str) : null))),
-
   tags: z.array(z.string()).nullable().optional().default([]),
   series: z.string().nullable().optional(),
   katex: z.boolean().optional(),
@@ -28,9 +28,9 @@ const thoughtSchema = z.object({
   ...basicSchema,
   date: z
     .date()
-    .or(z.string())
-    .transform((str) => (str ? new Date(str) : null)),
+    .or(z.string().transform((str) => (str ? new Date(str) : null))),
   tags: z.array(z.string()).nullable().optional().default([]),
+  images: z.array(z.string()).nullable().optional().default([]),
   reply: z.string().optional(),
   repost: z.string().optional(),
 });
@@ -38,7 +38,6 @@ const thoughtSchema = z.object({
 const goalSchema = z.object({
   ...basicSchema,
   title: z.string(),
-  archived: z.boolean().nullable().optional().default(false),
   archivedAt: z
     .date()
     .or(z.string())
@@ -59,6 +58,8 @@ const projectSchema = z.object({
 const pageSchema = z.object({
   ...basicSchema,
   title: z.string(),
+  url: z.string().nullable().default(""),
+  location: z.string().nullable().default("footer"),
 });
 
 const posts = defineCollection({
